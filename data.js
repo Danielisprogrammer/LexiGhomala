ff29dca2b417838ba3dbdbbd7d62f48dbdf98067
Danielisprogrammer <kenged776@gmail.com>
2026-02-25 15:28:47 +0100

    modified dictionnary.html

---
+++ data.js
@@ -0,0 +1,98 @@
+const dictionnaire = {
+    "bonjour" : "Mbôh",
+
+"bonsoir" : "Mbôh ndzié",
+
+"salut" : "Alo",
+
+"je vais bien" : "Ndzi mbôh",
+
+"merci" : "Mési",
+
+"merci beaucoup" : "Mési mbôh",
+
+"s’il te plaît" : "Tô ndzi",
+"excuse-moi" : "Pardon",
+
+"désolé" : "Pardon ndzi",
+
+"bienvenue" : "Yôh",
+
+"au revoir" : "Ndap",
+
+"a bientôt" : "Ndap mbôh",
+
+"bonne nuit" : "Mbôh ndzié",
+"bonne journée" : "Mbôh tsap",
+
+"paix" : "Ndzem",   
+
+"amour" : "Ndem",
+
+"unité" : "Kep",
+
+"respect" : "Ntsôh",
+"fraternité" : "Ndzôh",
+
+"solidarité" : "Tchop",
+
+"pardonnes-moi" : "Pardon ndzi mbôh",
+
+"que Dieu te bénisse" : "Si nda wé",
+
+"prends soin de toi" : "Ké ndzi wé",
+"soyons unis" : "Kep wé",
+
+"ordinateur" : "Ordinateur",
+
+"clavier" : "Klabie",
+
+"souris" : "Suris",
+
+"ecran" : "Ékran",
+"internet" : "Inténet",
+
+"réseau" : "Réseau",
+
+"mot de passe" : "Mot paase",
+
+"utilisateur" : "Utilisateu",
+
+"fichier" : "Fichie",
+"dossier" : "Dosie",
+
+"application" : "Application",
+
+"logiciel" : "Logiciel",
+
+"programme" : "Programme",
+
+"base de données" : "Base de données",
+"serveur" : "Serveur",
+
+"site web" : "Site web",
+
+"lien" : "Lien",
+
+"télécharger" : "Télécharger",
+"téléverser" : "Téléverser",
+"connexion" : "Connexion",
+
+"déconnexion" : "Déconnexion",
+
+"imprimer" : "Imprimer",
+
+"scanner" : "Scanner",
+
+"batterie" : "Batterie",
+"système" : "Système",
+
+"code" : "Code",
+
+"développeur" : "Développeur",
+
+"base" : "Base",   
+
+"données" : "Données",
+"numérique" : "Numérique",
+};
